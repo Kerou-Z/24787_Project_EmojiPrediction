93931c3c8d641d11d04fb16326cf41afe32a971c
Yongyi Zhao <70949720+YongyiZ98@users.noreply.github.com>
2020-11-24 18:21:20 +0800

    Update YongyizUpdate.py

--- YongyizUpdate.py
+++ YongyizUpdate.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 
 
 import pandas as pd
@@ -17,10 +13,6 @@ from keras.models import Sequential
 from keras.utils import to_categorical
 import math
 
-
-# In[ ]:
-
-
 def corpus_pre(X):  # numpy.ndarray 仅限train.csv的数据结构
     pred_X=[]
     stopper=set(stopwords.words('english'))
@@ -35,10 +27,6 @@ def corpus_pre(X):  # numpy.ndarray 仅限train.csv的数据结构
         pred_X.append(str)
     return pred_X  # 去除停止词后的 list
 
-
-# In[ ]:
-
-
 def dict_pre(file):  # wordembedding
     pred_dict={}
     f=open(file,"r",encoding='utf-8') # ['love',1 0 3 2 5 4 ]
@@ -50,18 +38,10 @@ def dict_pre(file):  # wordembedding
     f.close()
     return pred_dict
 
-
-# In[ ]:
-
-
 def tokenize(pred_corpus, l1):
     seqed_corpus = tokenizer.texts_to_sequences(pred_corpus)  # 将数据集序列化，就是把句中每一个单词编号
     X_out = sequence.pad_sequences(seqed_corpus, maxlen=l1, padding='post')  # 填充与截断序列，就是使得每句话对应的序列长度都是'maxlen'
     return X_out
-
-
-# In[ ]:
-
 
 def lookup(vocab, pred_dict):
     vocab_size = len(vocab) + 1 # 因为fit_on_texts时自动去除了1个最不常见的词
