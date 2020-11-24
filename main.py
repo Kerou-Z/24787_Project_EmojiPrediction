68b57c003464d717bfe04bd0c0baa501f508d4a5
jiarunw <jiarunw@andrew.cmu.edu>
2020-11-24 19:56:49 +0800

    main.py

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 
-def corpus_pre(X): # numpy.ndarray 仅限train.csv的数据结构
-    return pred_X # 去除停止词后的 list
-def dict_pre(file): # wordembedding
-    return pred_dict
-def lookup(pred_corpus,pred_dict):
-    return W # numpy.ndarray
-def model():
-    return 
-
+import math
+import pandas as pd
+import kerouz as kr
+import yongyizh as yy
+from keras.preprocessing.text import Tokenizer # https://keras-cn.readthedocs.io/en/latest/preprocessing/text/
+from keras.preprocessing import sequence
+from keras.utils import to_categorical
 
 if __name__=='__main__':
 
     # 可调参数
-    offset=2 # maxlen-average 
-    max_length = math.ceil(sum([len(s.split(" ")) for s in smoothtweets])/len(smoothtweets)) # 每句话平均长度
-    # l1:截取长度
-    # l2:平均长度
-    # l3:训练长度
-    # s.t. l1=l3=l2+offset
-    
-    # 读数据集
+    offset=2 # l1=l3=l2+offset. 其中l1:截取长度, l2:平均长度, l3:训练长度
+    rm_symbols='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+
+    # 读数据集和字典
     data=pd.read_csv('Train.csv')
-    x_train=data['TEXT'].values
+    X_train=data['TEXT'].values
+    Y_train=data['Label'].values
+    Y_train=to_categorical(Y_train)
+    f='glove.6B.50d.txt'
 
-    # 数据集预处理
-    pred_corpus=corpus_pre(X_train)
+    # 训练集去除特殊符号及stopwords
+    X_rm=yy.corpus_pre(X_train)
 
-    # Tokenizer
-    tokenizer = Tokenizer(filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', split=" ", lower=True) # filters：需要去除的符号 lower：转换成小写
-    tokenizer.fit_on_texts(pred_corpus) # 更新词库
-    seqed_corpus=tokenizer.texts_to_sequences(pred_corpus) # 将数据集序列化，就是把句中每一个单词编号
-    X_train=sequence.pad_sequences(seqed_corpus,maxlen=average+maxlen_offset,padding='post') # 填充与截断序列，就是使得每句话对应的序列长度都是'maxlen'
-    dict_pre(pred_corpus)
-    maxlength
-    vocab=tokenizer.word_index
+    # 分词及序列化
+    tokenizer = Tokenizer(filters=rm_symbols, split=" ", lower=True) # filters：需要去除的符号 lower：转换成小写
+    tokenizer.fit_on_texts(X_rm) # Tokenizer读取去除特殊符号的训练集,结果保存在tokenizer句柄内
+    X_seq=tokenizer.texts_to_sequences(X_rm) # 将数据集序列化就是把句中每一个单词编号
+
+    # 填充与截断
+    l2 = math.ceil(sum([len(s.split(" ")) for s in X_rm])/len(X_rm)) # l2:平均长度
+    X_pd=sequence.pad_sequences(X_seq,maxlen=l2+offset,padding='post') # 填充与截断序列，就是使得每句话对应的序列长度都是'maxlen'
+    X_ind=tokenizer.word_index
+
+    # 初始化字典
+    lookup_dict=yy.dict_pre(f)
+
+    # 根据字典生成weightMatrix
+    W=yy.lookup(X_ind,lookup_dict)
+
+    # 训练
+    model=kr.model_training(len(X_ind)+1, W, l2+offset, X_pd, Y_train, embed_dim=50, epochs=5, )
+    print(model.predict_classes(X_pd[1:13])) #test on some sentences in the train data set
--- readme.txt
+++
@@ -1,2 +0,0 @@
-dataset download link: https://www.kaggle.com/hariharasudhanas/twitter-emoji-prediction?select=Test.csv
-glove (embedding representations) download link: https://www.kaggle.com/anindya2906/glove6b
